d18b5e1df6286753c583de175bb164f38ea3c84c
Suzy Burgum <suzyburgum@gmail.com>
2025-04-01 13:52:05 -0400

    initialize React application with Vite, Tailwind CSS, and ESLint configuration

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl text-orange-500 font-bold">
+        welcome to awwwards
+      </h1>
+    </main>
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
